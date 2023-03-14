bd09f4dbcb2bb96ced1bfb05872e4c695c3dbc3c
Julian Aparicio <japaric4@uci.edu>
2023-03-14 00:23:33 -0700

    I also added the extract_json4 function in the ds_protocol class which goes through the json response and puts all the messages into a list which is returned back just like the usernames are.

--- test_ds_message_protocol.py
+++ test_ds_message_protocol.py
@@ -5,8 +5,8 @@ import ds_client
 class TestClient(unittest.TestCase):
  
     def test_true(self):
-        #self.assertEqual(ds_client.send("168.235.86.101", 3021, "123", "123", "hehe", "julianapa", "bio"), True)
-        self.assertEqual(ds_client.send_2("168.235.86.101", 3021, "julianapa", "1234", "new"), True)
+        self.assertEqual(ds_client.send("168.235.86.101", 3021, "123", "123", "hehe", "julianapa", "bio"), True)
+        #self.assertEqual(ds_client.send_2("168.235.86.101", 3021, "julianapa", "1234", "new"), True)
         #self.assertEqual(ds_client.send_2("168.235.86.101", 3021, "julianapa", "1234", "all"), True)
 if __name__ == '__main__':
     unittest.main()
